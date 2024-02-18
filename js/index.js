483f6037355a4c8683984934433e8965e30c8641
ethansy878 <esychangco@gmail.com>
2024-02-18 07:26:11 -0800

    Code cleanup index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,32 @@
-// https://www.reddit.com/r/flask/comments/afgks4/how_to_get_a_javascript_function_to_call_a_flask/
-
-
-// 127.0.0.1:5000/emission_calc?start="NULL"&end="NULL"&airlineName="Alaska"
-
+// Interaction with Flask Server
+// CREDIT: https://www.reddit.com/r/flask/comments/afgks4/how_to_get_a_javascript_function_to_call_a_flask/
 const sendPost = async (url) => {
-    // the URL to send the HTTP request to
-    //const body = ''; // whatever you want to send in the body of the HTTP request
-    const headers = {}
-    //{'Access-Control-Allow-Origin': '*','start': 'NULL', 'end': 'NULL', 'airlineName': "Alaska"}; // if you're sending JSON to the server
-    const method = 'GET';
+    const method = 'GET'; // not used
     const response = await fetch(url);
-    const data = await response.json(); // or response.json() if your server returns JSON
+    const emData = await response.json();
 
-    let emissionNum = document.getElementById('emissionnum')
-
-    /*setTimeout(function(){
-        console.log("Executed after 1 second");
-    }, 1000); */
-    
-    emissionNum.innerHTML = data + " kg CO2"
+    let emissionNum = document.getElementById('emissionnum')    
+    emissionNum.innerHTML = emData + " kg CO2"
 
     treeUrl = 'http://127.0.0.1:5000/trees?emissions=' + data
-    
     const treeResponse = await fetch(treeUrl)
-    const treeData = await treeResponse.json(); // or response.json() if your server returns JSON
+    const treeData = await treeResponse.json();
 
     let treeNum = document.getElementById('treenum')
     treeNum.innerHTML = treeData + " Trees I need to plant to offset your CO2 within a year"
 }
 
-//function for button to display photo
-
-
+// function for button to display photo
 function show_image(src, width, height, alt) {
     var img = document.createElement("img");
     img.src = src;
     img.width = width;
     img.height = height;
     img.alt = alt;
-
-    // This next line will just add it to the <body> tag
     document.body.appendChild(img);
 }
 
+// function for page redirect on "YES" press
 function happyRedirect(){
     let img = document.getElementById("mainimg")
     img.setAttribute("src", "./assets/HappyEM.png")
@@ -53,27 +37,21 @@ function happyRedirect(){
       }, 1000);
 }
 
-// https://dev.to/melvin2016/how-to-convert-an-html-string-into-real-html-or-dom-using-javascript-5992
-// make a new parser
-const parser = new DOMParser();
+// this must be global due to several function .this()
 let url = undefined;
 
-// https://stackoverflow.com/questions/11684454/getting-the-source-html-of-the-current-page-from-chrome-extension
+// CREDIT: https://stackoverflow.com/questions/11684454/getting-the-source-html-of-the-current-page-from-chrome-extension
 function onWindowLoad() {
-    let noButton = document.getElementById("NO")
+    let noButton = document.getElementById("NO");
     noButton.addEventListener("click", function() {
         show_image("./assets/SadEM.png", 100, 100, "Sadge")});
-    let yesButton = document.getElementById("YES")
+    let yesButton = document.getElementById("YES");
     yesButton.addEventListener("click", function() {
-            happyRedirect()});
+        happyRedirect()});
     
-        //noButton.setAttribute("height", noButton.getAttribute("width") + 100)
-
     let status = document.querySelector("#status");
     let idle = document.querySelector("#idle");
     let active = document.querySelector("#active");
-
-
 
     chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
         var activeTab = tabs[0];
@@ -83,20 +61,16 @@ function onWindowLoad() {
 
         return chrome.scripting.executeScript({
             target: { tabId: activeTabId },
-            // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
             func: DOMtoString,
-            // args: ['body']  // you can use this to target what element to get the html for
         });
 
     }).then(function (results) {
-        // logic here
         let doc = parser.parseFromString(results[0].result, "text/html");
 
         let startCode = undefined;
         let endCode = undefined;
         let airlineName = undefined
         let objs = {}
-
 
         if (url.includes("alaskaair")){
             idle.setAttribute("hidden", true);
